d5d79d77bc6026ff1342848403288d297e26a6ac
Christian Dupuis <cd@atomist.com>
2023-10-03 01:48:04 -0700

    Update base image and express

--- app.js
+++ app.js
@@ -4,34 +4,6 @@ const app = express()
 const port = 3000
 
 
-if (true) 
-  console.log("test")
-
-if (true) 
-  console.log("test")
-
-if (true) 
-  console.log("test")
-
-if (true) 
-  console.log("test")
-
-if (true) 
-  console.log("test")
-
-
-function func(a) {
-  let i = 10;
-  return i + a;
-  i++; // Noncompliant: this is never executed
-}
-
-function badstuff() {
-  const queryParams = new URLSearchParams(document.location.search);
-  const redirectUrl = queryParams.get("url");
-  document.location = redirectUrl; // Noncompliant
-}
-
 app.get('/', (req, res) => {
   res.send('Hello DockerCon 2023!');
 });

38f25c207470d2fded10527284254cfc49289cce
Christian Dupuis <cd@atomist.com>
2024-01-21 13:46:52 +0100

    Cleanup

--- app.js
+++ app.js
@@ -3,35 +3,6 @@ const express = require('express');
 const app = express()
 const port = 3000
 
-
-if (true) 
-  console.log("test")
-
-if (true) 
-  console.log("test")
-
-if (true) 
-  console.log("test")
-
-if (true) 
-  console.log("test")
-
-if (true) 
-  console.log("test")
-
-
-function func(a) {
-  let i = 10;
-  return i + a;
-  i++; // Noncompliant: this is never executed
-}
-
-function badstuff() {
-  const queryParams = new URLSearchParams(document.location.search);
-  const redirectUrl = queryParams.get("url");
-  document.location = redirectUrl; // Noncompliant
-}
-
 app.get('/', (req, res) => {
   res.send('Hello DockerCon 2023!');
 });
